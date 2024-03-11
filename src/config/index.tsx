de48bf431aa15410b6290efb93e32371c62e926f
dchernokur <d.chernokur@gmail.com>
2024-03-11 18:21:36 +0200

    feat: add walletconnect project id to env

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -4,10 +4,8 @@ import { cookieStorage, createConfig, createStorage, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { injected, walletConnect } from "@wagmi/connectors";
 
-//@TODO create prod id and move to env
 // Get projectId at https://cloud.walletconnect.com
-// export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
-export const projectId = "1c80b9743b6bb6c2e7e615647dde0c74";
+export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
 
 if (!projectId) throw new Error("Project ID is not defined");
 
