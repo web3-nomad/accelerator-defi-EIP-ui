edde99a6ef1716d980ae36cd223d76bfff107cea
Sergey Mushta <opengeekslabserv@gmail.com>
2024-04-03 14:24:40 +0300

    conf upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "out",
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
