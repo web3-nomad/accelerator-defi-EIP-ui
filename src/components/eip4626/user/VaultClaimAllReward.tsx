20e03d5bee7fdda6a2531d62c14589c1bbef147e
dchernokur <d.chernokur@gmail.com>
2024-05-28 13:39:49 +0200

    feat: add claim reward from vault error and result.

--- src/components/eip4626/user/VaultClaimAllReward.tsx
+++ src/components/eip4626/user/VaultClaimAllReward.tsx
@@ -1,4 +1,11 @@
-import { Button, Heading } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import { useWriteHederaVaultClaimAllReward } from "@/hooks/eip4626/mutations/useWriteHederaVaultClaimAllReward";
 import { VaultInfoProps } from "@/types/types";
 import { useReadHederaVaultCalculateReward } from "@/hooks/eip4626/useReadHederaVaultCalculateReward";
@@ -24,6 +31,21 @@ export function VaultClaimAllReward({ vaultAddress }: VaultInfoProps) {
       >
         Claim All Rewards
       </Button>
+
+      {claimResult && (
+        <Alert status="success">
+          <AlertIcon />
+          <AlertTitle>Claim success!</AlertTitle>
+          <AlertDescription>TxId: {claimResult}</AlertDescription>
+        </Alert>
+      )}
+      {claimError && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>Approve token error!</AlertTitle>
+          <AlertDescription>{claimError.toString()}</AlertDescription>
+        </Alert>
+      )}
     </>
   );
 }
